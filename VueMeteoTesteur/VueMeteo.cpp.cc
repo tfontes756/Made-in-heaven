de754077db69fbdfd2f1a674fbea12440846b5f7
tfontes <tfontes@btslivh.eu>
2019-12-19 17:30:57 +0100

    le truc

---
+++ VueMeteoTesteur/VueMeteo.cpp.cc
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   VueMeteo.cpp
+ * Author: snir2g2
+ *
+ * Created on 19 décembre 2019, 15:04
+ */
+
+#include "VueMeteo.h"
+
+VueMeteo::VueMeteo() {
+    widget.setupUi(this);
+}
+
+VueMeteo::~VueMeteo() {
+}
