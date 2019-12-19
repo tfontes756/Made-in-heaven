a75c6076b7c75da4956d6425d2ccd0dcc659aaa9
tfontes756 <58742395+tfontes756@users.noreply.github.com>
2019-12-20 00:17:46 +0100

    Update main.cpp

--- VueMeteoTesteur/main.cpp
+++ VueMeteoTesteur/main.cpp
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * File:   main.cpp
- * Author: snir2g2
- *
- * Created on 19 décembre 2019, 14:56
- */
 
 #include <QApplication>
 
